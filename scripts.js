de6cb8f75be77432b276df3fdfccea9f76b9f7fb
viviansdrv <vivian.sdrv@gmail.com>
2021-11-23 12:29:39 -0300

    commit from pge 23.11.21

--- scripts.js
+++ scripts.js
@@ -10,3 +10,43 @@ const Modal = {
         .classList.remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000, // vale 500.00, ele colocou "desformatado"
+        date: '23/01/2021'
+    },
+    {
+        id: 2,
+        description: 'Criação Website',
+        amount: 500000, // vale 500.00, ele colocou "desformatado"
+        date: '23/01/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000, // vale 500.00, ele colocou "desformatado"
+        date: '23/01/2021'
+    },
+]
+
+const Transaction = {
+
+}
+
+const DOM = {
+    innerHTMLTransaction(){
+        const html = `
+                    <tr>
+                        <td class="description">Luz</td>
+                        <td class="expense">- R$ 500,00</td>
+                        <td class="date">23/01/2021</td>            
+                        <td>
+                            <img src="./assets/minus.svg" alt="Remover transação">
+                        </td>            
+                    </tr>
+                    `
+    }
+}
