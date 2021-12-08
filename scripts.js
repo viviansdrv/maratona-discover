e7b4f74557ea292e5d5e201b7e39144dbac8a376
viviansdrv <83466012+viviansdrv@users.noreply.github.com>
2021-12-08 00:59:51 -0300

    casa 7.12.2021

--- scripts.js
+++ scripts.js
@@ -30,23 +30,37 @@ const transactions = [
         amount: -20000, // vale 500.00, ele colocou "desformatado"
         date: '23/01/2021'
     },
+    {
+        id: 4,
+        description: 'APP',
+        amount: 23000, // vale 500.00, ele colocou "desformatado"
+        date: '24/01/2021'
+    },
 ]
-
-const Transaction = {
-
-}
-
 const DOM = {
-    innerHTMLTransaction(){
-        const html = `
-                    <tr>
-                        <td class="description">Luz</td>
-                        <td class="expense">- R$ 500,00</td>
-                        <td class="date">23/01/2021</td>            
-                        <td>
-                            <img src="./assets/minus.svg" alt="Remover transação">
-                        </td>            
-                    </tr>
-                    `
+            transactionsContainer: document.querySelector('#data-table tbody'),
+
+            addTransaction(transaction, index){
+            const tr = document.createElement('tr')
+            tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+            DOM.transactionsContainer.appendChild(tr)
+        },
+        innerHTMLTransaction(transaction){
+            const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+            const html = `
+                            <td class="description">${transaction.description}</td>
+                            <td class="${CSSclass}">${transaction.amount}</td>
+                            <td class="date">${transaction.date}</td>            
+                            <td>
+                                <img src="./assets/minus.svg" alt="Remover transação">
+                            </td>            
+                        `
+            return html
     }
 }
+// DOM.addTransaction(transactions[0])
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
